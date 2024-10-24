278a839614aec627f1f2fc480e052a7fa9de9cf4
Luca Seggiani <seggianiluca2004@gmail.com>
2024-10-24 20:36:56 +0200

    First commit

---
+++ 1/maiusc_string.s
@@ -0,0 +1,47 @@
+# leggere un messaggio da terminale
+# convertire le lettere maiuscole in minuscole
+# stampare il messaggio modificato
+# stavolta con le istruzioni di stringa
+
+.include "./files/utility.s"
+
+.data
+msg_in: .fill 80, 1, 0
+msg_out: .fill 80, 1, 0
+
+.text
+_main:
+	nop
+
+.primo: # leggi stringa
+	mov $80, %cx
+	lea msg_in, %ebx
+	call inline
+
+.secondo: # converti string
+	lea msg_in, %esi
+	lea msg_out, %edi
+	
+	cld
+
+loop:
+	lodsb
+
+	cmp $'a', %al
+	jb dopo
+	cmp $'z', %al
+	ja dopo
+
+	and $0xdf, %al # 1101 1111
+
+dopo:
+	stosb
+
+	cmp $'\r', %al
+	jne loop
+	
+.terzo: # stampa stringa
+	lea msg_out, %ebx
+	call outline
+
+	ret
---
+++ README.md
@@ -0,0 +1,2 @@
+# Esercizi assembler
+Uso questa repo per caricare i miei esercizi di _Assembler_ (si chiamerebbe _Assembly_, io do la colpa all'uvettà isterica)
